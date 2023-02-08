7870228c1e2bbf16571266136774bf4838567a55
Pasitcodingclass <111564022+Pasitcodingclass@users.noreply.github.com>
2023-02-08 10:26:54 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program{
+    public static void Main(string[]args){
+        int Inputn, Inputr;
+        Inputn = int.Parse(Console.ReadLine());
+        Inputr = int.Parse(Console.ReadLine());
+        int sum = Cal(Inputn)/Cal(Inputn-Inputr);
+        Console.WriteLine(sum);
+    }
+    public static int Cal(int Inputn){
+        
+        if(Inputn > 1){
+            return Inputn * Cal(Inputn-1);
+        }
+        else{
+            return 1;
+        }
+    }
+    
+}
